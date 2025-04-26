1462ab3f471bd4cc80d9ee5d7c969385f15b8643
Lucia Vu <luciavu6@outlook.com>
2025-04-27 00:11:59 +1000

    fix: fix error display message

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -2,12 +2,15 @@ const db = require('../../db/queries');
 const CustomNotFoundError = require('../../errors/CustomNotFoundError.js');
 
 module.exports = {
-  get: async (req, res) => {
+  get: async (req, res, next) => {
     try {
       const messages = await db.getAllMessages();
+      if (!messages) {
+        return next(new CustomNotFoundError('Could not retrieve messages.'));
+      }
       res.render('index', { title: 'Mini Message Board', messages: messages });
     } catch (err) {
-      throw new CustomNotFoundError('Could not retrieve messages.');
+      next(err);
     }
   },
 };
--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,13 +1,16 @@
 const db = require('../../db/queries');
 const CustomNotFoundError = require('../../errors/CustomNotFoundError.js');
 
-async function getMessageById(req, res) {
+async function getMessageById(req, res, next) {
   const { messageId } = req.params;
   try {
     const message = await db.getMessageById(messageId);
+    if (!message || message.length === 0) {
+      return next(new CustomNotFoundError('Message not found.'));
+    }
     res.render('messageDetails', { title: 'Message Details', message: message[0] });
   } catch (err) {
-    throw new CustomNotFoundError('Message not found.');
+    next(err);
   }
 }
 module.exports = {
